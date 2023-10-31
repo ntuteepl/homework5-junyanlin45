dcc3d6f11781118bd596e5b2b43f90cf2432ab30
junyanlin45 <146065138+junyanlin45@users.noreply.github.com>
2023-10-31 15:56:06 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int H,M,A,m;
+    float h ,t;
+    scanf("%d%d",&H,&M);
+    h=H*30+M*0.5;
+    m=M*6;
+
+    if(h>m)
+    {
+        t=h-m;
+        if(t>180)
+        {
+            t=360-t;
+        }
+            printf("%.3f",t);
+    }
+    if(m>h)
+    {
+        t=m-h;
+        if(t>180)
+        {
+            t=360-t;
+        }
+        printf("%.3f",t);
+    }
+
+}
 
